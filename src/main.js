1b5440d28c53a1fd7683191bcaa3319caf91389d
Austin Chen <akrolsmir@gmail.com>
2021-06-09 21:16:01 -0400

    Replace ElementUI with ElementPlus

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import Vue, { createApp } from 'vue';
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import { createApp } from 'vue';
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
 import App from './example/App.vue';
 
-Vue.use(ElementUI);
-
-createApp(App).mount('#app');
+const app = createApp(App);
+app.use(ElementPlus);
+app.mount('#app');
